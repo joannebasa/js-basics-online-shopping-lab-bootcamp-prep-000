e537e8324076957102ad5e049ecdd66b397c5680
Joanne Basa <joannegbasa@gmail.com>
2017-11-03 06:15:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function removeFromCart(item) {
   return cart;
 }
 
-function placeOrder(cardNumber) {
+var placeOrder = cardNumber => {
   if (cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.");
   }
